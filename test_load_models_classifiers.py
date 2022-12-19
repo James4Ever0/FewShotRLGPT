0a7ec2232b4371232db8f24046a26c719aa111c9
James4Ever0 <randomvoidmail@foxmail.com>
2022-12-20 04:31:17 +0800

    test on loading models
    describe general directions and purposes

---
+++ test_load_models_classifiers.py
@@ -0,0 +1,7 @@
+# whatever. let's load models.
+# something in our mind: PPO, supervised, setfit as RM, language models which can take actions (webgpt-like), autonomous learning (seek goals itself), human interface automation (check terminal and display, control mouse, keyboard), assistive language models, AGI (maybe later. let's first use the LM to understand the doc and blog!)
+
+## you want to write this in pytest compatible tests? cause in that way you don't have to write try...except every time.
+
+def test_load_model():
+  # how to unload the model?
